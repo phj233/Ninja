f37d5f034f0c956bd8aeb3bb2a9321e070d7b814
phj233 <2780990934@qq.com>
2023-07-07 00:52:53 +0800

    build: updating dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
-import AsyncCatch from 'vite-plugin-async-catch'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -10,7 +9,6 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-    AsyncCatch({ catchCode: `console.error(e)` }),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
